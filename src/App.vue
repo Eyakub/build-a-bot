79b67dfe9e4d1f151ca464654e1fb1e37d087d55
eyakub <eyakubsorkar@gmail.com>
2020-04-14 23:24:54 +0600

    init and home page done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="main">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img src="./assets/build-a-bot-logo.png" alt="" class="logo" >
+            Build a Bot
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <HomePage msg="Welcome to Your Vue.js App"/>
+    </main>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
 
 <style>
+body{
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+
+header{
+  background-color: #999;
+  width: 1048px;
+  margin: 0 auto;
+}
+
+ul {
+  padding: 3px;
+  display: flex;
+}
+
+.nav-item{
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <div>
+      <img
+        class="robot"
+        src="../assets/robot-home.png"
+        alt=""
+        aria-hidden="true"
+      />
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+.robot {
+  height: 300px;
+}
+</style>

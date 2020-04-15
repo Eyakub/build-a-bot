4bd83a89aba70fb659c1aff024b2543f5eb4d861
eyakub <eyakubsorkar@gmail.com>
2020-04-15 23:50:13 +0600

    card option complete

--- src/components/build/RobotBuilder.vue
+++ src/components/build/RobotBuilder.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
+  <div class="content">
+    <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
     <div class="top-row">
       <div class="top part">
-        <img :src="availableParts.heads[selectedHeadIndex].src" alt title="head" />
+        <div class="robot-name">
+          {{ selectedRobot.head.title}}
+          <span v-if="selectedRobot.head.onSale" class="sale">Sale</span>
+        </div>
+        <img :src="selectedRobot.head.src" alt title="head" />
         <button class="prev-selector" @click="selectPreviousHead()">&#9668;</button>
         <button class="next-selector" @click="selectNextHead()">&#9658;</button>
       </div>
@@ -10,19 +15,19 @@
 
     <div class="middle-row">
       <div class="left part">
-        <img :src="availableParts.arms[selectedLeftArmIndex].src" alt title="left arm" />
+        <img :src="selectedRobot.leftArm.src" alt title="left arm" />
         <button class="prev-selector" @click="selectPreviousLeftArm()">&#9650;</button>
         <button class="next-selector" @click="selectNextLeftArm()">&#9660;</button>
       </div>
 
       <div class="center part">
-        <img :src="availableParts.torsos[selectedTorsoIndex].src" title="left arm" alt />
+        <img :src="selectedRobot.torso.src" title="left arm" alt />
         <button class="prev-selector" @click="selectPreviousTorso()">&#9668;</button>
         <button class="next-selector" @click="selectNextTorso()">&#9658;</button>
       </div>
 
       <div class="right part">
-        <img :src="availableParts.arms[selectedRightArmIndex].src" alt title="left arm" />
+        <img :src="selectedRobot.rightArm.src" alt title="left arm" />
         <button class="prev-selector" @click="selectPreviousRightArm()">&#9650;</button>
         <button class="next-selector" @click="selectNextRightArm()">&#9660;</button>
       </div>
@@ -30,10 +35,27 @@
 
     <div class="bottom-row">
       <div class="bottom part">
-        <img :src="availableParts.bases[selectedBaseIndex].src" alt title="head" />
+        <img :src="selectedRobot.base.src" alt title="head" />
         <button class="prev-selector" @click="selectPreviousBase()">&#9668;</button>
         <button class="next-selector" @click="selectNextBase()">&#9658;</button>
       </div>
+    </div>
+    <div>
+      <h1>Cart</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Robot</th>
+            <th class="cost">Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for='(robot, index) in cart' :key='index'>
+            <td>{{robot.head.title}}</td>
+            <td class="cost">{{ robot.cost }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -45,6 +67,7 @@ export default {
   name: "RobotBuilder",
   data() {
     return {
+      cart: [],
       availableParts,
       selectedHeadIndex: 0,
       selectedLeftArmIndex: 0,
@@ -52,6 +75,18 @@ export default {
       selectedRightArmIndex: 0,
       selectedBaseIndex: 0
     };
+  },
+
+  computed: {
+    selectedRobot() {
+      return {
+        head: availableParts.heads[this.selectedHeadIndex],
+        leftArm: availableParts.arms[this.selectedLeftArmIndex],
+        torso: availableParts.torsos[this.selectedTorsoIndex],
+        rightArm: availableParts.arms[this.selectedRightArmIndex],
+        base: availableParts.bases[this.selectedBaseIndex]
+      };
+    }
   },
 
   methods: {
@@ -106,7 +141,7 @@ export default {
       );
     },
 
-    // TORSO 
+    // TORSO
     selectNextTorso() {
       this.selectedTorsoIndex = this.getNextValidIndex(
         this.selectedTorsoIndex,
@@ -132,6 +167,21 @@ export default {
         this.selectedBaseIndex,
         availableParts.bases.length
       );
+    },
+
+    addToCart() {
+      const robot = this.selectedRobot;
+      const cost =
+        robot.head.cost +
+        robot.leftArm.cost +
+        robot.torso.cost +
+        robot.rightArm.cost +
+        robot.base.cost;
+      
+      // as we got the robot object, so adding the cost variable to the robot obj
+      // this following way
+      this.cart.push(Object.assign({}, robot, { cost }))
+      console.log(this.cart)
     }
   }
 };

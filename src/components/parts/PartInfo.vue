840898904b3b6122adb0c2c4c4ab61888ced37ed
eyakub <eyakubsorkar@gmail.com>
2020-04-18 11:17:20 +0600

    nested routing on vue done

--- src/components/parts/PartInfo.vue
+++ src/components/parts/PartInfo.vue
@@ -6,25 +6,25 @@
 </template>
 
 <script>
-import parts from '../../data/parts';
+import parts from "../../data/parts";
 export default {
-	name: "PartInfo",
-	props: {
-		partType: {type: String},
-		id: {
-			type: [Number, String],
-			validator(value){
-				return Number.isInteger(Number(value));
-			}
-		}
-	},
-  computed: {
-		part() {
-				const { partType, id } = this; // as we get this as props
-				// const { partType, id } = this.$route.params.partType;
-				// +id means type cast the id variable
-				return parts[partType].find(part => part.id === +id);
-		}
+  name: "PartInfo",
+  props: {
+    partType: { type: String },
+    id: {
+      type: [Number, String],
+      validator(value) {
+        return Number.isInteger(Number(value));
+      }
+    }
   },
+  computed: {
+    part() {
+      const { partType, id } = this; // as we get this as props
+      // const { partType, id } = this.$route.params.partType;
+      // +id means type cast the id variable
+      return parts[partType].find(part => part.id === +id);
+    }
+  }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,11 @@ import Router from 'vue-router';
 import HomePage from '../components/home/HomePage.vue';
 import RobotBuilder from '../components/build/RobotBuilder.vue';
 import PartInfo from '../components/parts/PartInfo.vue';
+import BrowseParts from '../components/parts/BrowseParts.vue';
+import RobotHeads from '../components/parts/RobotHeads.vue';
+import RobotArms from '../components/parts/RobotArms.vue';
+import RobotTorsos from '../components/parts/RobotTorsos.vue';
+import RobotBases from '../components/parts/RobotBases.vue';
 
 Vue.use(Router);
 
@@ -16,6 +21,29 @@ export default new Router({
     path:'/build',
     name: 'Build',
     component: RobotBuilder,
+  },{
+    path: '/parts/browse',
+    name: 'BrowseParts',
+    component: BrowseParts,
+    children: [
+      {
+        name: 'BrowseHeads',
+        path: 'heads',
+        component: RobotHeads,
+      },{
+        name: 'BrowseArms',
+        path: 'arms',
+        component: RobotArms,
+      },{
+        name: 'BrowseTorsos',
+        path: 'torsos',
+        component: RobotTorsos,
+      },{
+        name: 'BrowseBases',
+        path: 'bases',
+        component: RobotBases,
+      }
+    ]
   },{
     path:'/parts/:partType/:id',
     name: 'Parts',
